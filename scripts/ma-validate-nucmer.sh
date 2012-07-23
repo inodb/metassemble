2e8b911cde91de447f629639555a1338c432da6e
Ino de Bruijn <ino.debruijn@scilifelab.se>
2012-07-23 22:17:20 +0200

    Added a script that maps an assembly back to the reference using nucmer and then checks the metagenomic statistics using R.

---
+++ scripts/ma-validate-nucmer.sh
@@ -0,0 +1,27 @@
+set -o errexit
+set -o nounset
+# From: http://tinyurl.com/85qrydz
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPTDIR/global-functions.incl
+
+if [[ -f $1 ]]; then
+    REF=$1
+else
+    echo "$1 is not a file" >&2
+    exit 1
+fi
+if [[ -f $2 ]]; then
+    QUERY=$2
+else
+    echo "$2 is not a file" >&2
+    exit 1
+fi
+OUTPUTBASE=$3
+
+check_prog nucmer show-coords
+
+nucmer --maxmatch --prefix=$OUTPUTBASE $REF $QUERY
+# delta-filter 
+show-coords -rclTH $OUTPUTBASE.delta > $OUTPUTBASE.coords
+Rscript $SCRIPTDIR/stats/ma-stats-nucmer-coords.R $QUERY $OUTPUTBASE.coords \
+    > $OUTPUTBASE.validate
