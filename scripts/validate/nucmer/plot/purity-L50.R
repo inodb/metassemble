e2b8e401328fdd57fe8fe80420f0d4a4c12f2924
Ino de Bruijn <ino.debruijn@scilifelab.se>
2012-09-25 14:24:30 +0200

    Most important fix was the incorrect folder copying of velveth output in Makefile. Velvet rule and Metavelvet rule were both wrong, fortunately didn't perform any assemblies with that rule yet. Problem lies in that it is difficult/impossible to specify multiple output files for one rule (http://www.cmcrossroads.com/cm-basics/12905-rules-with-multiple-outputs-in-gnu-make).

--- scripts/validate/nucmer/plot/purity-L50.R
+++ scripts/validate/nucmer/plot/purity-L50.R
@@ -9,14 +9,6 @@ suppressMessages({
     library(ggplot2)
 })
 
-maxstring <- function(string, cutoff) {
-    if (nchar(string) > cutoff) {
-        paste(sep="","...",substr(string,nchar(string)-cutoff+1,nchar(string)))
-    } else {
-        string
-    }
-}
-
 # Parse arguments
 parse_arguments <- function() {
   option_list <- list(make_option(c("-g", "--gradient-color-column"), type="character",
@@ -39,18 +31,28 @@ if (length(args) < 2) {
 output.pdf <- args[[1]]
 
 assemblies <- read.table(args[[2]],header=TRUE)
-assemblies$name <- sapply(as.character(assemblies$name), maxstring, 50)
-
-print(levels(assemblies$asm_type))
-print(assemblies$kmer_type[match(levels(assemblies$asm_type),assemblies$asm_type)])
-print(as.numeric(assemblies$kmer_type[match(sort(levels(assemblies$asm_type)),assemblies$asm_type)]))
 
 # Plot the assembly
 pdf(output.pdf)
 if (length(opts[['gradient-color-column']]) == 1) {
-    g <- ggplot(data=assemblies,aes(x=as.numeric(l50),y=as.numeric(purity),size=as.integer(assemblies[,opts[['gradient-color-column']]]),color=asm_type,shape=asm_type))
-    g <- g + geom_point() 
-    g <- g + scale_size_continuous(name=opts[['gradient-color-column']]) + scale_colour_discrete("Assembly type") + scale_shape_manual("Assembly type",values=c(1,20)[as.numeric(assemblies$kmer_type[match(sort(levels(assemblies$asm_type)),assemblies$asm_type)])])
+    # Velvet circles, metavelvet diamonds
+    shapes <- c(20,20,20,20,18,18,18,18)
+    names(shapes) <- c('velvetscaf',
+        'newblervelvetnoscaf','velvetnoscaf','minimus2velvetnoscaf',
+        'metavelvetscaf','metavelvetnoscaf','minimus2metavelvetnoscaf','newblermetavelvetnoscaf')
+    # Color blind scale from:
+    # http://wiki.stdout.org/rcookbook/Graphs/Colors%20%28ggplot2%29/
+    # http://jfly.iam.u-tokyo.ac.jp/color/
+    # noscaf black, scaf yellowish, minimus2noscaf blue, newblernoscaf pink 
+    colors <- c("#E69F00","#CC79A7","#000000","#0072B2","#E69F00","#000000","#0072B2","#CC79A7")
+    names(colors) <- names(shapes)
+
+    g <- ggplot(data=assemblies,aes(x=as.numeric(l50),y=as.numeric(purity),size=assemblies[,opts[['gradient-color-column']]],color=asm_type,shape=asm_type,fill=asm_type))
+    g <- g + geom_point() +
+        scale_size_continuous(name=opts[['gradient-color-column']],range=c(1,6)) +
+        scale_colour_manual("Assembly",values=colors[levels(assemblies$asm_type)]) +
+        scale_fill_manual("Assembly",values=colors[levels(assemblies$asm_type)]) +
+        scale_shape_manual("Assembly",values=shapes[levels(assemblies$asm_type)])
 } else {
     g <- ggplot(data=assemblies,aes(x=as.numeric(l50),y=as.numeric(purity),color=asm_type))
     g <- g + geom_point() + scale_colour_discrete("Assembly")
