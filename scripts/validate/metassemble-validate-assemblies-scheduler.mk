00847b9d00649610d15950b9d76298aa8e978abf
Ino de Bruijn <ino.debruijn@scilifelab.se>
2014-03-17 13:25:08 +0100

    Add reference validation with broken up contigs.

--- scripts/validate/metassemble-validate-assemblies-scheduler.mk
+++ scripts/validate/metassemble-validate-assemblies-scheduler.mk
@@ -1,12 +1,20 @@
-REF?=/bubo/home/h16/inod/glob/metagenomics/result/chris-mock/Project_ID793_dAmore/Sample_50ng_even/mapping/velvet35test/references.fa
-PHYL_REF?=/bubo/home/h16/inod/glob/github/masm-vali/masmvali/test/data/chris-mock/reference/phylogeny-references.tsv
-MAKEFILE_VALIDATE?=Makefile-validate-assemblies
-
 ifndef METASSEMBLE_DIR
 $(error METASSEMBLE_DIR environment variable not set. Set with export METASSEMBLE_DIR=...)
 endif
 include $(METASSEMBLE_DIR)/scripts/metassemble.mk
 include $(METASSEMBLE_DIR)/lib/scheduler.mk
+
+ifndef REF
+$(error REF variable not set. Set like REF=/path/to/references.fa)
+endif
+ifndef PHYL_REF
+$(error PHYL_REF variable not set. Set like PHYL_REF=/path/to/references.fa)
+endif
+ifndef MAKEFILE_VALIDATE
+$(error MAKEFILE_VALIDATE variable not set. Set like MAKEFILE_VALIDATE=/path/to/references.fa)
+endif
+
+include $(ASSEMBLY_MAKEFILE)
 
 ################################
 # --------- ref stats ---------#
@@ -20,6 +28,8 @@ $(OUT)/reference-stats/ref.stats: $(FASTQ1) $(FASTQ2) $(REF)
 ################################
 # --------- nucmer ------------#
 ################################
+%/val/nucmer.coords: %/bambus2.scaffold.linear.fasta $(REF)
+	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_NUCMER_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
 %/val/nucmer.coords: %/$(CONTIG_FILENAME) $(REF)
 	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_NUCMER_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
 %/val/nucmer.coords: %/$(SCAF_FILENAME) $(REF)
@@ -27,11 +37,13 @@ $(OUT)/reference-stats/ref.stats: $(FASTQ1) $(FASTQ2) $(REF)
 %/val/nucmer.coords: %/$(MERGE_FILENAME) $(REF)
 	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_NUCMER_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
 
-%/val/asm-stats.tsv: %/val/nucmer.coords $(OUT)/reference-stats/ref.stats $(PHYL_REF) %/$(CONTIG_FILENAME)
+%/val/asm-stats.tsv: %/bambus2.scaffold.linear.fasta %/val/nucmer.coords $(REF) $(PHYL_REF) $(CON_TO_REF) $(OUT)/reference-stats/ref.stats
 	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_MASMVALI_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
-%/val/asm-stats.tsv: %/val/nucmer.coords $(OUT)/reference-stats/ref.stats $(PHYL_REF) %/$(SCAF_FILENAME)
+%/val/asm-stats.tsv: %/$(CONTIG_FILENAME) %/val/nucmer.coords $(REF) $(PHYL_REF) $(CON_TO_REF) $(OUT)/reference-stats/ref.stats
 	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_MASMVALI_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
-%/val/asm-stats.tsv: %/val/nucmer.coords $(OUT)/reference-stats/ref.stats $(PHYL_REF) %/$(MERGE_FILENAME)
+%/val/asm-stats.tsv: %/$(SCAF_FILENAME) %/val/nucmer.coords $(REF) $(PHYL_REF) $(CON_TO_REF) $(OUT)/reference-stats/ref.stats
+	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_MASMVALI_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
+%/val/asm-stats.tsv: %/$(MERGE_FILENAME) %/val/nucmer.coords $(REF) $(PHYL_REF) $(CON_TO_REF) $(OUT)/reference-stats/ref.stats
 	$(call schedule_with_deps_and_store_id,$(SCHEDULER_STD_OPT) $(SCHEDULER_MASMVALI_OPT),make -ef $(MAKEFILE_VALIDATE) $@)
 ################################
 # ---------/nucmer ------------#
@@ -55,8 +67,15 @@ $(OUT)/reference-stats/ref.stats: $(FASTQ1) $(FASTQ2) $(REF)
 # is not able to get the other assemblies to complete.
 .PHONY:
 validateexisting: \
+	$(subst bambus2.scaffold.linear.fasta,val/asm-stats.tsv,\
 	$(subst $(MERGE_FILENAME),val/asm-stats.tsv,\
 	$(subst $(SCAF_FILENAME),val/asm-stats.tsv,\
-	$(subst $(CONTIG_FILENAME),val/asm-stats.tsv,$(wildcard $(ALLASMCONTIGS) $(ALLASMSCAFFOLDS)))))
+	$(subst $(CONTIG_FILENAME),val/asm-stats.tsv,$(wildcard $(ALLASMCONTIGS) $(ALLASMSCAFFOLDS))))))
+.PHONY:
+validateall: \
+	$(subst bambus2.scaffold.linear.fasta,val/asm-stats.tsv,\
+	$(subst $(MERGE_FILENAME),val/asm-stats.tsv,\
+	$(subst $(SCAF_FILENAME),val/asm-stats.tsv,\
+	$(subst $(CONTIG_FILENAME),val/asm-stats.tsv,$(ALLASMCONTIGS) $(ALLASMSCAFFOLDS)))))
 
 .PRECIOUS: %/val/nucmer.coords
